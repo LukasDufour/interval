a0369824ab03b92d42b46f913f6186582a10d0e9
Dufour <Dufour@DESKTOP-5452IJF>
2022-03-14 22:10:41 +0100

    API

--- interval/src/interval/Interval.java
+++ interval/src/interval/Interval.java
@@ -1,28 +1,94 @@
 package interval;
 
+/**
+ * Each instance of this class represents an interval.
+ * 
+ * @invar De lengte is gelijk aan de bovengrens min de ondergrens.
+ *     | getLength() == getBovengrens() - getOndergrens()
+ * @invar De bovengrens is niet kleiner dan de ondergrens
+ *     | getOndergrens() <= getBovengrens()
+ */
 
 public class Interval {
-	int ondergrens;
-	int lengte;
+	/**
+	 * @invar De lengte is niet negatief
+	 *     | 0 <= lengte
+	 */
+	private int lengte;
+	private int bovengrens;
 	
-	int getOndergrens() {
-		return this.ondergrens;
+	public int getOndergrens() {
+		return bovengrens - lengte;
 	}
 	
-	int getBovengrens() {
-		return this.ondergrens + this.lengte;
+	public int getBovengrens() {
+		return bovengrens;
 	}
 	
-	void setOndergrens(int waarde) {
-		this.ondergrens = waarde;
+	/**
+	 * @post | result == getBovengrens() - getOndergrens()
+	 */
+	public int getLength() {
+		return lengte;
 	}
 	
-	void setBovengrens(int waarde) {
-		this.lengte = waarde - this.ondergrens;
+	/**
+	 * @post | getOndergrens() == 0
+	 * @post | getBovengrens() == 0
+	 */
+	public Interval() {
+		this.lengte = 0;
+		this.bovengrens = 0;
 	}
 	
-	int getLength() {
-		return this.lengte;
+	/**
+	 * @post | getOndergrens() == ondergrens
+	 * @post | getLength() == lengte
+	 */	
+	public Interval(int ondergrens, int lengte) {
+		this.lengte = lengte;
+		this.bovengrens = ondergrens + lengte;
+	}
+	
+	/**
+	 * Stelt de ondergrens in.
+	 * Laat de bovengrens ongewijzigd.
+	 * 
+	 * @throws IllegalArgumentException als de waarde groter is dan de bovengrens
+	 *     | getBovengrens() < waarde
+	 * 
+	 * @post De ondergrens is gelijk aan de gegeven waarde.
+	 *     | getOndergrens() == waarde
+	 *     
+	 * @mutates_properties | this.getOndergrens()
+	 */
+	
+	public void setOndergrens(int waarde) {
+		if (bovengrens < waarde)
+			throw new IllegalArgumentException("`waarde` is groter dan de bovengrens");
+		lengte = bovengrens - waarde;
+	}
+	
+	/**
+	 * Stelt de bovengrens in.
+	 * Laat de ondergrens ongewijzigd.
+	 * 
+	 * @throws IllegalArgumentException
+	 *     | waarde < getOndergrens()
+	 * 
+	 * @post De bovengrens is gelijk aan de gegeven waarde.
+	 *     | getBovengrens() == waarde
+	 * @post De ondergrens blijft ongewijzigd
+	 *     | getOndergrens() == old(getOndergrens())
+	 * 
+	 * @mutates | this
+	 */
+	
+	public void setBovengrens(int waarde) {
+		if (waarde < bovengrens - lengte)
+			throw new IllegalArgumentException("`waarde` is kleinder dan de ondergrens");
+		lengte = waarde - (bovengrens - lengte);
+		bovengrens = waarde;
 	}
 	
 }
