21aee11e99d4ebd19abc3a780e0137a4f24d527a
Dufour <Dufour@DESKTOP-5452IJF>
2022-03-14 18:52:24 +0100

    Using instance methods

--- interval/src/interval/Interval.java
+++ interval/src/interval/Interval.java
@@ -1,5 +1,28 @@
 package interval;
 
-public class Interval {
 
+public class Interval {
+	int ondergrens;
+	int lengte;
+	
+	int getOndergrens() {
+		return this.ondergrens;
+	}
+	
+	int getBovengrens() {
+		return this.ondergrens + this.lengte;
+	}
+	
+	void setOndergrens(int waarde) {
+		this.ondergrens = waarde;
+	}
+	
+	void setBovengrens(int waarde) {
+		this.lengte = waarde - this.ondergrens;
+	}
+	
+	int getLength() {
+		return this.lengte;
+	}
+	
 }
--- interval/src/interval/IntervalTest.java
+++ interval/src/interval/IntervalTest.java
@@ -6,39 +6,14 @@ import org.junit.jupiter.api.Test;
 
 class IntervalTest {
 	
-	class Interval {
-		int ondergrens;
-		int lengte;
-	}
-	
-	int getOndergrens(Interval interval) {
-		return interval.ondergrens;
-	}
-	
-	int getBovengrens(Interval interval) {
-		return interval.ondergrens + interval.lengte;
-	}
-	
-	void setOndergrens(Interval interval, int waarde) {
-		interval.ondergrens = waarde;
-	}
-	
-	void setBovengrens(Interval interval, int waarde) {
-		interval.lengte = waarde - interval.ondergrens;
-	}
-	
-	int getLength(Interval interval) {
-		return interval.lengte;
-	}
-	
 	@Test
 	void testInterval() {
 		Interval interval = new Interval();
-		setOndergrens(interval, 0);
-		setBovengrens(interval, 2);
-		assertEquals(0, getOndergrens(interval));
-		assertEquals(2, getBovengrens(interval));
-		assertEquals(2, getLength(interval));
+		interval.setOndergrens(0);
+		interval.setBovengrens(2);
+		assertEquals(0, interval.getOndergrens());
+		assertEquals(2, interval.getBovengrens());
+		assertEquals(2, interval.getLength());
 	}
 
 }
